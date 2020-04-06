605a0d7db4ca35a85473883b90c39039f08247ea
hn-88 <hn-88@users.noreply.github.com>
2020-04-06 17:09:01 +0530

    fbo runs at 2 fps
    Only for buffer size smaller than screen size.

--- GL_warp2mp4.h
+++ GL_warp2mp4.h
@@ -16,12 +16,27 @@ GLvoid OnKeyPress (unsigned char key, GLint x, GLint y);
 GLvoid OnIdle();
 
 bool DrawGL();
+
+// from fbo
+
+int screenWidth, screenHeight;
+bool mouseLeftDown;
+bool mouseRightDown;
+float mouseX, mouseY;
+float cameraAngleX;
+float cameraAngleY;
+float cameraDistance;
+const float CAMERA_DISTANCE = 6.0f;
+
+
+
 void GenerateMovie();
 void onExitCleanup();
 void CreateGrid();
 void CreateGridNoColor();
 bool ReadMesh(std::string strpathtowarpfile);
 void PreCreateGrid();
+void getNextFrame();
 
 VideoCapture inputVideo; 
 VideoWriter outputVideo;
@@ -33,17 +48,29 @@ int outputw = 1920;
 int outputh = 1080;
 int windoww = 800;
 int windowh = 600;
+int texturew = 2048;
+int textureh = 2048;
 char *pixels;
+int returncode;
 
-// PBO
+// PBO, FBO
 GLuint pboIds[2];                   // IDs of PBO
-GLuint textureId;                   // ID of texture
+GLuint fbotextureId, srctextureId;  // IDs of textures
 GLubyte* imageData = 0;             // pointer to texture buffer
+GLuint fboId;                       // ID of FBO
+
+GLuint rboColorId, rboDepthId;      // IDs of Renderbuffer objects
 bool pboSupported;
 int pboMode = 2;
 int drawMode = 0;
+bool fboSupported;
+bool fboUsed;
+
 int    CHANNEL_COUNT   = 3;
 int    DATA_SIZE;    //    = outputw * outputh * CHANNEL_COUNT;
+int TEXTURE_WIDTH = 2048, TEXTURE_HEIGHT = 2048;
+// Hack, make this a variable.
+
 
 // from GL_warp2Avi
 uint nFrames;
